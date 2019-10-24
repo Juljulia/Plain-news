c77faaaac9a49f9d5f2b53e44f794c35178d0b85
Julia Karlsson <karlssonjulia.m@gmail.com>
2019-10-24 13:23:02 +0200

    Update data.php

--- data.php
+++ data.php
@@ -15,7 +15,7 @@ $articles = [
     ],
 
     [
-        'title' => 'Mupsim lorem',
+        'title' => 'Diam sit',
         'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
         'author' => '',
         'publishedDate' => 20190418,    
@@ -23,7 +23,15 @@ $articles = [
     ],
 
     [
-        'title' => 'Mupsim lorem',
+        'title' => 'Adipiscing',
+        'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
+        'author' => '',
+        'publishedDate' => 20190419,    
+        'likes' => 7,
+    ],
+
+    [
+        'title' => 'suscipit',
         'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
         'author' => '',
         'publishedDate' => 20190419,    
@@ -39,7 +47,7 @@ $articles = [
     ],
 
     [
-        'title' => 'Mupsim lorem',
+        'title' => 'Semper lorem',
         'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
         'author' => '',
         'publishedDate' => 20190419,    
@@ -47,7 +55,7 @@ $articles = [
     ],
 
     [
-        'title' => 'Mupsim lorem',
+        'title' => 'Quis lorem',
         'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
         'author' => '',
         'publishedDate' => 20190419,    
@@ -55,7 +63,7 @@ $articles = [
     ],
 
     [
-        'title' => 'Mupsim lorem',
+        'title' => 'volutpat',
         'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
         'author' => '',
         'publishedDate' => 20190419,    
@@ -63,7 +71,7 @@ $articles = [
     ],
 
     [
-        'title' => 'Mupsim lorem',
+        'title' => 'Lorem ipsum',
         'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
         'author' => '',
         'publishedDate' => 20190419,    
@@ -71,15 +79,7 @@ $articles = [
     ],
 
     [
-        'title' => 'Mupsim lorem',
-        'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
-        'author' => '',
-        'publishedDate' => 20190419,    
-        'likes' => 7,
-    ],
-
-    [
-        'title' => 'Mupsim lorem',
+        'title' => 'Diamoro',
         'content' => 'Diam sit amet nisl suscipit adipiscing. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Molestie a iaculis at erat pellentesque. Vulputate enim nulla aliquet porttitor lacus luctus. Amet tellus cras adipiscing enim eu turpis egestas pretium. Libero enim sed faucibus turpis in eu mi bibendum. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor commodo ullamcorper a lacus vestibulum. A arcu cursus vitae congue mauris. Amet purus gravida quis blandit turpis. Mattis enim ut tellus elementum.' ,
         'author' => '',
         'publishedDate' => 20190419,    
